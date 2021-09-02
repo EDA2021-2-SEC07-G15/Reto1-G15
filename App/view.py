2eff9c88ad86c7d339a9ae561495e3c3284f8d28
Oscar Garcia <oi.garcia@uniandes.edu.co>
2021-09-01 21:39:26 -0500

    View

--- App/view.py
+++ App/view.py
@@ -36,8 +36,14 @@ operación solicitada
 
 def printMenu():
     print("Bienvenido")
-    print("1- Cargar información en el catálogo")
-    print("2- ")
+    print("1 - Cargar información en el catálogo.")
+    print("2 - Listar cronológicamente los artistas.")
+    print("3 - Listar cronológicamente las adquisiciones.")
+    print("4 - Clasificar las obras de un artista por técnica.")
+    print("5 - Clasificar las obras por la nacionalidad de sus creadores.")
+    print("6 - Transportar obras de un departamento.")
+    print("7 - Proponer una nueva exposición en el museo.")
+    print("Salir")
 
 catalog = None
 
